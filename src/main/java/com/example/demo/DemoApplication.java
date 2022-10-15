e40ce065ed5138e42657c8aeb129c0c978e02b06
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2022-10-16 03:11:28 +0530

    2. CREATED HELLO WORLD API FOR TESTING - Commented out JPA dependency as it is not connected to database.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController // Used to create restfull web services
+
+// From Here only we start our application
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/") // Get Mapping used to return back something
+	public String hello() {
+		return "Hello World";
+	}
+
+	@GetMapping("/json")
+	public List<String> helloList() {
+		return List.of("Hello", "World");
+	}
 }
