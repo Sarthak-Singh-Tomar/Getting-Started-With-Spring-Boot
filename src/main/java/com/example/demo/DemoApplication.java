72a43fa5b2c3b09c83e73ec15ceb2ffab92b9f8c
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2022-10-16 03:33:38 +0530

    4. CREATED A REST ENDPONT - RETURNS STUDENT DETAILS - Created inside the same main Application class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,13 +20,28 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/") // Get Mapping used to return back something
-	public String hello() {
-		return "Hello World";
+//	@GetMapping("/") // Get Mapping used to return back something
+//	public String hello() {
+//		return "Hello World";
+//	}
+//
+//	@GetMapping("/json")
+//	public List<String> helloList() {
+//		return List.of("Hello", "World");
+//	}
+
+	@GetMapping("/")
+	public List<Student> students() {
+		return List.of(
+				new Student(
+						1L,
+						"john",
+						"john@abc.com",
+						LocalDate.of(2000, Month.JANUARY, 2),
+						21
+				)
+		);
 	}
 
-	@GetMapping("/json")
-	public List<String> helloList() {
-		return List.of("Hello", "World");
-	}
+
 }
